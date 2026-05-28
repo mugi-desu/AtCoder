9355bcf4d42457c6fd09d7615810d253f2147486
mugi-desu <mugimugidaga@gmail.com>
2026-05-28 12:17:58 +0900

    piture

---
+++ Picture.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int H,W;
+    cin >> H >> W;
+    string str;
+    for(int i=0;i<H+2;i++){
+        if(i==0 || i==H+1){
+            for(int j=0;j<W+2;j++){
+                cout << "#";
+            }
+            cout << endl;
+        }
+        else{
+            for(int j=0;j<3;j++){
+                if(j==0 || j==2){
+                    cout << "#";
+                }
+                else{
+                    cin >> str;
+                    cout << str;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AtCoder
-
+整数型　int
+実数　double
+文字　char
+文字列 string
+真偽　bool
+配列　vector<型>
