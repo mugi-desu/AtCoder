49c7a2e500d4291281a3a68a178b8cb18288e5eb
mugi-desu <mugimugidaga@gmail.com>
2026-05-29 14:56:44 +0900

    kaiketsu

--- 086CTravel.cpp
+++ 086CTravel.cpp
@@ -2,26 +2,35 @@
 using namespace std;
 
 int main(){
-    int N;
+    int N,a=0,b=0,c=0;
     cin >> N;
     for(int i=0;i<N;i++){
         int t,x,y;
         cin >> t >> x >> y;
-        if(x+y > t){
+        if(x<a){
+            swap(x,a);
+        }
+        if(y<b){
+            swap(y,b);
+        }
+        if(x-a+y-b > t-a){
             cout << "No" << endl;
             return 0;
         }
-        if(t%2 == 0){
-            if((x+y)%2==1){
+        if((t-a)%2 == 0){
+            if((x-a+y-b)%2==1){
                 cout << "No" << endl;
                 return 0;
             }
             continue;
         }
-        if((x+y)%2==0){
+        if((x-a+y-b)%2==0){
             cout << "No" << endl;
             return 0;
         }
+        a=t;
+        b=x;
+        c=y;
     }
     cout << "Yes" << endl;
 }
